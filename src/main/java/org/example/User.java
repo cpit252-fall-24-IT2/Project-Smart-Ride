2680cd15960ab14b73ebd88177341d361229a9a5
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-10-29 16:00:16 +0300

    User Class

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class User {
+    private String username;
+
+    private double balance;
+
+    public User(String username) {
+        this.username = username;
+        this.balance = 0.0;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username='" + username + "', balance=" + balance + "}";
+    }
+}
