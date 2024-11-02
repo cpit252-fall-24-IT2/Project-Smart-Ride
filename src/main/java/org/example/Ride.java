7f5c2056b12888d0c702cb0c156756b5200ac2fb
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-11-03 00:01:53 +0300

    Ride Class
    Ride Class Completed

--- src/main/java/org/example/Ride.java
+++ src/main/java/org/example/Ride.java
@@ -1,4 +1,64 @@
 package org.example;
 
+import java.util.Date;
+
 public class Ride {
+    private User passenger;
+    private User driver;
+    private String pickupLocation;
+    private String dropoffLocation;
+    private Date dateTime;
+    private boolean completed;
+
+    public Ride(User passenger) {
+        this.passenger = passenger;
+    }
+
+    public void setPassenger(User passenger) {
+        this.passenger = passenger;
+    }
+
+    public User getPassenger() {
+        return passenger;
+    }
+
+    public void setDriver(User driver) {
+        this.driver = driver;
+    }
+
+    public User getDriver() {
+        return driver;
+    }
+
+    public void setPickupLocation(String location) {
+        this.pickupLocation = location;
+    }
+
+    public String getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public void setDropoffLocation(String location) {
+        this.dropoffLocation = location;
+    }
+
+    public String getDropoffLocation() {
+        return dropoffLocation;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void completeRide() {
+        this.completed = true;
+    }
 }
