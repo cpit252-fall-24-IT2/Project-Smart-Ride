f1af014679e771e244307e8c764575e2985d4275
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-11-03 13:10:10 +0300

    OfferRide Class

--- src/main/java/org/example/OfferRide.java
+++ src/main/java/org/example/OfferRide.java
@@ -1,6 +1,26 @@
 package org.example;
 
 public class OfferRide {
-    public OfferRide(String preferredDateTime, int availableSeats, double price) {
+    private String preferredDateTime;
+    private int availableSeats;
+    private double PricePerSeat;
+    private String carType;
+
+    //constructor
+    public OfferRide(String preferredDateTime, int availableSeats, double pricePerSeat, String carType) {
+        this.preferredDateTime = preferredDateTime;
+        this.availableSeats = availableSeats;
+        PricePerSeat = pricePerSeat;
+        this.carType = carType;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferRide{" +
+                ", preferredDateTime='" + preferredDateTime + '\'' +
+                ", availableSeats=" + availableSeats +
+                ", PricePerSeat=" + PricePerSeat +
+                ", carType='" + carType + '\'' +
+                '}';
     }
 }
