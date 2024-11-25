0cb04afbb0071904328eaf245c6a07d848d4bf9e
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-11-25 20:30:40 +0300

    updating pom.xml

--- src/main/java/org/example/MapService.java
+++ src/main/java/org/example/MapService.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.awt.*;
 import java.util.List;
 
 public interface MapService {
     String getCoordinates(String address);
     String getAddress(double latitude, double longitude);
     List<String> findNearbyPlaces(double latitude, double longitude, String placeType);
+    Component displayMap(double latitude, double longitude, int zoomLevel);
+    Component showCurrentLocation();
 }
