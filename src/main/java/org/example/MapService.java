99ee74bb9664ef1618efedb5a4ce90c5bc867e58
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-11-25 20:34:31 +0300

    changing the MapService
    changing to be more in line with the JxMapViewer API we want to use

--- src/main/java/org/example/MapService.java
+++ src/main/java/org/example/MapService.java
@@ -4,9 +4,12 @@ import java.awt.*;
 import java.util.List;
 
 public interface MapService {
-    String getCoordinates(String address);
-    String getAddress(double latitude, double longitude);
-    List<String> findNearbyPlaces(double latitude, double longitude, String placeType);
-    Component displayMap(double latitude, double longitude, int zoomLevel);
-    Component showCurrentLocation();
+    
+    void addMarker(double lat, double lon, String label);
+
+    void zoomIn();
+
+    void zoomOut();
+
+    void centerMap(double lat, double lon);
 }
