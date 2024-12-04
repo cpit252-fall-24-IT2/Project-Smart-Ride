3dce6747b8088358fc96302c7c1b975bc37fcf71
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-12-04 14:18:05 +0300

    Update TwilioAdapter.java

--- src/main/java/org/example/SMS/TwilioAdapter.java
+++ src/main/java/org/example/SMS/TwilioAdapter.java
@@ -2,10 +2,16 @@ package org.example.SMS;
 
 import com.twilio.*;
 
-public class TwilioAdapter {
+public class TwilioAdapter implements SMSHander{
 
     public static final String ACCOUNT_SID = "";
     public static final String AUTH_TOKEN = "";
 
+    public TwilioAdapter() {
+    }
 
+    @Override
+    public boolean sendVerifyCode(String Phone, String Code) {
+        return false;
+    }
 }
