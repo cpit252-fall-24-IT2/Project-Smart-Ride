ae677d764e735cfdb63b715f8738f6a63817b748
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2024-12-09 20:39:06 +0300

    basic server

--- src/main/java/org/example/Server/DemoApplication.java
+++ src/main/java/org/example/Server/DemoApplication.java
@@ -1,9 +1,38 @@
 package org.example.Server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class DemoApplication {
 
     public static void main(String[] args) {
-        
+        final int port = 12345;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server started. Listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected from " + clientSocket.getInetAddress());
+
+                new Thread(new ClientHandler(clientSocket)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 
 }
